e328df6dec52df91c57337bcf9bae44b3d6d282f
koushik-ch <55940631+koushik-ch@users.noreply.github.com>
2022-02-15 10:05:25 +0530

    Time: 26 ms (55.88%), Space: 16.9 MB (50.83%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int xorSum=0;
+        
+        for(int num:nums){
+            xorSum^=num;
+        }
+        return xorSum;
+    }
+};
