296043df7acae397a8d1114523274eb20809119b
koushik-ch <55940631+koushik-ch@users.noreply.github.com>
2022-03-26 11:04:47 +0530

    Time: 6 ms (77.51%), Space: 7.2 MB (68.05%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,56 +1,42 @@
 class Solution {
 public:
+    
     vector<vector<string>> solveNQueens(int n) {
+        vector<int> ld(2*n,0);
+        vector<int> ud(2*n,0);
+        vector<int> rowHash(n,0);
+        
         vector<vector<string>> ans;
         
         vector<string> board(n,string(n,'.'));
         
-        solve(n,ans,board,0);
+        solve(n,ans,board,0,ld,ud,rowHash);
         return ans;
     }
     
-    void solve(int n,vector<vector<string>>& ans,vector<string>& board,int col){
+    void solve(int n,vector<vector<string>>& ans,vector<string>& board,int col,vector<int>& ld,vector<int>& ud,vector<int>& rowHash){
         if(col==n){
             ans.push_back(board);
             return;
         }
         
         for(int row=0;row<n;row++){
-            if(isValid(board,row,col,n)){
+            if(!rowHash[row]&&!ld[row+col]&&!ud[n+(row-col)]){
                 board[row][col]='Q';
-                solve(n,ans,board,col+1);
+                
+                rowHash[row]=1;
+                ld[row+col]=1;
+                ud[n+(row-col)]=1;
+                
+                solve(n,ans,board,col+1,ld,ud,rowHash);
                 board[row][col]='.';
+                
+                rowHash[row]=0;
+                ld[row+col]=0;
+                ud[n+(row-col)]=0;
             }
         }
     }
     
-    bool isValid(vector<string>& board,int row,int col,int n){
-        int rowCopy=row;
-        int colCopy=col;
-        
-        while(row>=0&&col>=0){
-            if(board[row][col]=='Q')
-                return false;
-            row--;
-            col--;
-        }
-        row=rowCopy;
-        col=colCopy;
-        
-        while(col>=0){
-            if(board[row][col]=='Q')
-                return false;
-            col--;
-        }
-        
-        col=colCopy;
-        
-        while(col>=0&&row<n){
-            if(board[row][col]=='Q')
-                return false;
-            row++;
-            col--;
-        }
-        return true;
-    }
+
 };
