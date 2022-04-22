793415f29592c844269b10db22b496ad5f5d6ffc
koushik-ch <55940631+koushik-ch@users.noreply.github.com>
2022-04-22 21:14:38 +0530

    Time: 13 ms (95.71%), Space: 11.1 MB (54.73%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string res;
+        
+        for(char c:s){
+            if(res.length()&&res.back()==c)
+                res.pop_back();
+            else
+                res.push_back(c);
+        }
+        return res;
+    }
+};
