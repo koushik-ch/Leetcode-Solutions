09d8e42635f6b983317c2f6e03f839f07cc7c696
koushik-ch <55940631+koushik-ch@users.noreply.github.com>
2022-05-05 17:43:49 +0530

    Time: 201 ms (63.92%), Space: 69.7 MB (67.89%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
@@ -2,16 +2,27 @@ class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
         if(arr.size()&1) return false;
-        unordered_map<int,int>m;
-        for(auto x:arr) m[(x%k + k)%k]++;          //store the count of remainders in a map.
-        for(auto x:arr)
-        {
-            int rem=(x%k + k)%k;
-            if(rem==0)                         //if the remainder for an element is 0 then the count of numbers that give this remainder must be even.
-            { 
-                if(m[rem] & 1) return false;            //if count of numbers that give this remainder is odd all pairs can't be made hence return false.
-            }         
-            else if(m[rem] != m[k - rem]) return false;    //if the remainder rem and k-rem do not have the same count then pairs can not be made 
+        
+        unordered_map<int,int> mp;
+        
+        for(int num:arr){
+            int mod=(num%k+k)%k;
+            
+            mp[mod]++;
+        }
+        
+        for(int num: arr){
+            
+            int mod=(num%k+k)%k;
+            
+            if(mod==0){
+                if(mp[mod]&1)
+                    return false;
+            }
+            else{
+                if(mp[mod]!=mp[k-mod])
+                    return false;
+            }
         }
         return true;
     }
