5ae0071c5f53c7699429ded4b04a5eceafe11f41
koushik-ch <55940631+koushik-ch@users.noreply.github.com>
2022-05-09 17:55:38 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (95.19%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+        if(nums.size()==1)
+            return nums[0];
+        // vector<int> dp(nums.size()+1);
+        int val1=0,val2=nums[0];
         
-        vector<int> dp(nums.size()+1);
-        
-        dp[0]=nums[0];
+        // dp[0]=nums[0];
+        int curri=0;
         
         for(int i=1;i<nums.size();i++){
-            int val1=nums[i];
-            if(i>1)
-                val1+=dp[i-2];
             
-            dp[i]=max(val1,dp[i-1]);
+            val1+=nums[i];
+            curri=max(val1,val2);
+            
+            val1=val2;
+            val2=curri;
+            
+            
+//             int val1=nums[i];
+//             if(i>1)
+//                 val1+=dp[i-2];
+            
+//             dp[i]=max(val1,dp[i-1]);
         }
-        return dp[nums.size()-1];
+        return curri;
         
-        return robHelper(nums,nums.size()-1);
+        // return dp[nums.size()-1];
+        
+        
     }
     
-    int robHelper(vector<int>& nums,int ind){
-        if(ind<0)
-            return 0;
-        
-        if(ind==0)
-            return nums[0];
-        
-        int val1=nums[ind]+robHelper(nums,ind-2);
-        int val2=robHelper(nums,ind-1);
-        
-        return max(val1,val2);
-    }
+   
 };
