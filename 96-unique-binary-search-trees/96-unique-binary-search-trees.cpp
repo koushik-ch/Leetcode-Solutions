722a414b3f9da99b7bb5556a4bb2fe6507ed151c
koushik-ch <55940631+koushik-ch@users.noreply.github.com>
2022-05-11 14:39:54 +0530

    Time: 4 ms (21.42%), Space: 6 MB (37.44%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     int numTrees(int n) {
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
+    }
+    
+    int solve(int n,vector<int>& dp){
         if(n==0)
             return 1;
         if(n<=2)
             return n;
+        if(dp[n]!=-1)
+            return dp[n];
+        
         int ans=0;
         
         for(int i=1;i<=n;i++){
-            ans+=numTrees(i-1)*numTrees(n-i);
+            ans+=solve(i-1,dp)*solve(n-i,dp);
         }
-        return ans;
+        
+        return dp[n]=ans;
     }
 };
