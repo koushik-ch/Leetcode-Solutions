7fff867e40e5f2e438c1ac3d9b446e4afefa9d01
koushik-ch <55940631+koushik-ch@users.noreply.github.com>
2022-07-08 20:50:20 +0530

    Time: 19 ms (5.23%), Space: 12 MB (74.70%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n=nums.size();
-        int breakPt=-1;
-        
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                breakPt=i;
-                break;
-            }
+        int breakPoint = -1;
+    int n = nums.size();
+    for (int i = n - 2; i >= 0; i--)
+    {
+        if (nums[i] < nums[i + 1])
+        {
+            breakPoint = i;
+            break;
         }
-        if(breakPt<0){
-            reverse(nums.begin(),nums.end());
-            return;
+    }
+
+    if (breakPoint == -1)
+    {
+        reverse(nums.begin(), nums.end());
+        return;
+    }
+
+    for (int i = n - 1; i > breakPoint; i--)
+    {
+        if (nums[i] > nums[breakPoint])
+        {
+            swap(nums[i], nums[breakPoint]);
+            break;
         }
-        
-        for(int i=n-1;i>=breakPt;i--){
-            if(nums[i]>nums[breakPt]){
-                swap(nums[breakPt],nums[i]);
-                break;
-            }
-        }
-        
-        reverse(nums.begin()+breakPt+1,nums.end());
+    }
+    reverse(nums.begin() + breakPoint + 1, nums.end());
+
+    for (int i = 0; i < n; i++)
+        cout << nums[i] << " ";
     }
 };
