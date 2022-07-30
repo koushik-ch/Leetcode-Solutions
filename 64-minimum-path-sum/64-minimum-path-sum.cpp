3c0abca26917ad237ff18efbc986e3159b2ffd61
koushik-ch <55940631+koushik-ch@users.noreply.github.com>
2022-07-30 10:19:09 +0530

    Time: 22 ms (20.20%), Space: 10.1 MB (29.03%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -2,23 +2,27 @@ class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size(),n=grid[0].size();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        return memoize(m-1,n-1,m,n,grid,dp);
-       
+        
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        
+        dp[0][0]=grid[0][0];
+        
+        for(int i=1;i<m;i++){
+            dp[i][0]=dp[i-1][0]+grid[i][0];
+        }
+        
+        for(int i=1;i<n;i++){
+            dp[0][i]=dp[0][i-1]+grid[0][i];
+        }
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                int left=dp[i][j-1];
+                int up=dp[i-1][j];
+                
+                dp[i][j]=min(left,up)+grid[i][j];
+            }
+        }
+        return dp[m-1][n-1];
     }
-    
-    int memoize(int i,int j,int m,int n,vector<vector<int>>& grid,vector<vector<int>>& dp){
-    if(i<0||j<0||i>=m||j>=n)
-        return INT_MAX-1;
-    if(i==0&&j==0)
-        return grid[0][0];
-    if(dp[i][j]!=-1)
-        return dp[i][j];
-    int left=memoize(i,j-1,m,n,grid,dp);
-    int up=memoize(i-1,j,m,n,grid,dp);
-    int val=min(left,up);
-    
-    return dp[i][j]=val+grid[i][j];
-    
-}
 };
