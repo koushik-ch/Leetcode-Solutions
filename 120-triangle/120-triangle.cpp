9873ada65bef6e8343f6d25e1eac6bdb8d042f93
koushik-ch <55940631+koushik-ch@users.noreply.github.com>
2022-07-30 13:19:27 +0530

    Time: 6 ms (84.07%), Space: 9.2 MB (7.91%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int i=triangle.size();
-        int j=triangle[i-1].size();
-        vector<vector<int>> dp(i,vector<int>(j,-1));
-        
-        
-        return solve(triangle,0,0,dp);
-        
-    }
     
-    int solve(vector<vector<int>>& triangle,int i,int j,vector<vector<int>>& dp){
-        if(i>=triangle.size())
-            return 0;
-        
-        if(j>=triangle[i].size())
-            return INT_MAX;
+    int memoize(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>& dp){
+        if(j>i)
+            return INT_MAX-1;
+        if(i==triangle.size()-1)
+            return triangle[i][j];
         
         if(dp[i][j]!=-1)
             return dp[i][j];
         
-        return dp[i][j]=min(solve(triangle,i+1,j,dp),solve(triangle,i+1,j+1,dp))+triangle[i][j];
+        int same=0,next=0;
+        same=memoize(i+1,j,triangle,dp);
+        next=memoize(i+1,j+1,triangle,dp);
         
+        return dp[i][j]=min(same,next)+triangle[i][j];
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        
+        return memoize(0,0,triangle,dp);
     }
 };
