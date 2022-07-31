2210de06c366fb91b14d2583beabc302ebd20ca5
koushik-ch <55940631+koushik-ch@users.noreply.github.com>
2022-07-31 14:13:01 +0530

    Time: 6 ms (84.08%), Space: 9.5 MB (7.96%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     
     int memoize(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>& dp){
-        if(j>i)
-            return INT_MAX-1;
+       
         if(i==triangle.size()-1)
             return triangle[i][j];
         
@@ -16,10 +15,27 @@ public:
         
         return dp[i][j]=min(same,next)+triangle[i][j];
     }
+    
+    int tabulation(int n,vector<vector<int>> triangle){
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        
+        for(int i=0;i<n;i++)
+            dp[n-1][i]=triangle[n-1][i];
+        
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down=dp[i+1][j];
+                int diagonal=dp[i+1][j+1];
+                
+                dp[i][j]=min(diagonal,down)+triangle[i][j];
+            }
+        }
+        return dp[0][0];
+    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
         vector<vector<int>> dp(n,vector<int>(n,-1));
-        
+        return tabulation(n,triangle);
         return memoize(0,0,triangle,dp);
     }
 };
