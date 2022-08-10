2c767f4d8963e9f0c1fe9accdcbd2563a4a0ab9f
koushik-ch <55940631+koushik-ch@users.noreply.github.com>
2022-08-10 09:11:58 +0530

    Time: 15 ms (37.73%), Space: 13 MB (78.80%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -2,56 +2,46 @@ class Solution {
 public:
     
     int orangesRotting(vector<vector<int>>& grid) {
-        
-        queue<pair<int,int>> q;
-        
-        vector<int> change={-1,0,1,0,-1};
-        
-        int orangeCount=0;
         int m=grid.size(),n=grid[0].size();
-        
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1)
-                    orangeCount++;
-                
-                if(grid[i][j]==2)
-                    q.push({i,j});
-            }
+    int freshCount=0,timeTaken=0;
+    queue<pair<int,int>> q;
+    
+    vector<int> delta={-1,0,1,0,-1};
+    
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(grid[i][j]==2)
+                q.push({i,j});
+            else if(grid[i][j]==1)
+                freshCount++;
         }
+    }
+    
+    while(!q.empty()&&freshCount>0){
+        int size=q.size();
         
-        int minutes=-1;
-        if(orangeCount==0)
-            return 0;
-        while(!q.empty()){
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                
-            pair<int,int> curr=q.front();
-        
+        for(int i=0;i<size;i++){
+            pair<int,int> currRottenApple=q.front();
             q.pop();
             
-            for(int i=0;i<4;i++){
-                int x=curr.first+change[i];
-                int y=curr.second+change[i+1];
-                if(x<0||y<0||x==m||y==n||grid[x][y]!=1)
-                    continue;
-                q.push({x,y});
-                grid[x][y]=2;
-                 orangeCount--;
-            }
-            // grid[curr.first][curr.second]=2;
-           
+            for(int x=0;x<4;x++){
+                int dx=currRottenApple.first+delta[x];
+                int dy=currRottenApple.second+delta[x+1];
                 
+                if(dx<0||dx>=m||dy<0||dy>=n||grid[dx][dy]!=1)
+                    continue;
+                q.push({dx,dy});
+                grid[dx][dy]=2;
+                freshCount--;
             }
-            minutes++;
-            
         }
-        
-        if(orangeCount)
-            return -1;
-        
-        return minutes;
-        
+        timeTaken++;
+    }
+    
+    
+    if(freshCount>0)
+        return -1;
+    
+    return timeTaken;
     }
 };;
