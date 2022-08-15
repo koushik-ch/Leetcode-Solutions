1db257c08b23eb4a42660222ce51b3242061fb7f
koushik-ch <55940631+koushik-ch@users.noreply.github.com>
2022-08-15 21:57:11 +0530

    Time: 57 ms (18.82%), Space: 13.2 MB (22.07%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    
+    int memoize(int ind1,int ind2,string s1,string s2,vector<vector<int>>& dp){
+        if(ind1<0||ind2<0)
+            return 0;
+        
+        if(ind1==0&&ind2==0)
+            return s1[ind1]==s2[ind2];
+        
+        if(dp[ind1][ind2]!=-1)
+            return dp[ind1][ind2];
+        
+        if(s1[ind1]==s2[ind2])
+            return dp[ind1][ind2]=memoize(ind1-1,ind2-1,s1,s2,dp)+1;
+        
+        return dp[ind1][ind2]=max(memoize(ind1-1,ind2,s1,s2,dp),memoize(ind1,ind2-1,s1,s2,dp));
+        
+    }
+    
+    int tabulation(int m1,int m2,string s1,string s2){
+        vector<vector<int>> dp(m1+1,vector<int>(m2+1,0));
+        
+//         if(s1[0]==s2[0])
+//             dp[0][0]=1;
+        
+//         for(int i=0;i<m1;i++)
+//             dp[i][0]=s1[0]==s2[0];
+//         for(int j=0;j<m2;j++)
+//             dp[0][j]=s1[0]==s2[0];
+        
+        for(int i=1;i<=m1;i++){
+            for(int j=1;j<=m2;j++){
+                
+                if(s1[i-1]==s2[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                
+            }
+        }
+        
+        return dp[m1][m2];
+        
+    }
+    
+    int longestCommonSubsequence(string s1, string s2) {
+        int m1=s1.length(),m2=s2.length();
+        
+        return tabulation(m1,m2,s1,s2);
+        vector<vector<int>> dp(m1+1,vector<int>(m2+1,-1));
+        
+        return memoize(m1-1,m2-1,s1,s2,dp);
+    }
+};
