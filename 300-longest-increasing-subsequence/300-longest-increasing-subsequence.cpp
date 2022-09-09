1b7f7753cdbf3ff19b76c041f02dd80ad584e108
koushik-ch <55940631+koushik-ch@users.noreply.github.com>
2022-09-09 17:31:19 +0530

    Time: 2649 ms (5.00%), Space: 292.5 MB (8.63%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
+    int memoization(vector<int>& nums,int i,int prevInd,vector<vector<int>>& dp){
+        if(i==nums.size())
+            return 0;
+        
+        if(dp[i][prevInd+1]!=-1)
+            return dp[i][prevInd+1];
+        
+        int notTake=memoization(nums,i+1,prevInd,dp);
+        int take=0;
+        
+        if(prevInd==-1||nums[i]>nums[prevInd])
+            take=memoization(nums,i+1,i,dp)+1;
+        
+        
+        return dp[i][prevInd+1]=max(take,notTake);
+    }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
         
-        vector<int> dp(n+1,-1);
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
         
-        return solve(nums,0,INT_MIN,dp);
+        return memoization(nums,0,-1,dp);
     }
     
-    int solve(vector<int>& nums,int ind,int prev,vector<int>& dp){
-        if(ind>=nums.size()||nums[ind]<=prev)
-            return 0;
-        
-        if(dp[ind]!=-1)
-            return dp[ind];
-        
-        int ans=0;
-        
-       for(int i=ind+1;i<=nums.size();i++){
-            ans=max(ans,solve(nums,i,nums[ind],dp)+1);
-            ans=max(ans,solve(nums,i,prev,dp));
-       }
-        return dp[ind]=ans;
-    }
 };
