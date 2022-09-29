7774b1566111ba14649fbfc833ff10162e642ac8
koushik-ch <55940631+koushik-ch@users.noreply.github.com>
2022-09-29 09:10:15 +0530

    Time: 27 ms (38.09%), Space: 9.9 MB (92.10%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    void solve(vector<vector<char>>& board) {
-        int m=board.size(),n=board[0].size();
+    void solve(vector<vector<char>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
         
         for(int i=0;i<m;i++){
-            if(board[i][0]=='O')
-                dfs(board,i,0,m,n);
-            if(board[i][n-1]=='O')
-                dfs(board,i,n-1,m,n);
+            if(grid[i][0]=='O')
+                dfs(grid,i,0,m,n);
+            if(grid[i][n-1]=='O')
+                dfs(grid,i,n-1,m,n);
         }
         
         for(int j=0;j<n;j++){
-            if(board[0][j]=='O')
-                dfs(board,0,j,m,n);
-            if(board[m-1][j]=='O')
-                dfs(board,m-1,j,m,n);
+            if(grid[0][j]=='O')
+                dfs(grid,0,j,m,n);
+            if(grid[m-1][j]=='O')
+                dfs(grid,m-1,j,m,n);
         }
         
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(board[i][j]=='#')
-                    board[i][j]='O';
-                else if(board[i][j]=='O')
-                    board[i][j]='X';
+                if(grid[i][j]=='.')
+                    grid[i][j]='O';
+                else if(grid[i][j]=='O')
+                    grid[i][j]='X';
             }
         }
     }
@@ -30,10 +31,45 @@ public:
     void dfs(vector<vector<char>>& board,int i,int j,int m,int n){
         if(i<0||j<0||i>=m||j>=n||board[i][j]!='O')
             return;
-        board[i][j]='#';
+        
+        board[i][j]='.';
+        
+        dfs(board,i,j+1,m,n);
+        dfs(board,i,j-1,m,n);
         dfs(board,i+1,j,m,n);
         dfs(board,i-1,j,m,n);
-        dfs(board,i,j-1,m,n);
-        dfs(board,i,j+1,m,n);
     }
 };
+
+
+
+
+
+
+
+
+
+ //We will use boundary DFS to solve this problem
+        
+      // Let's analyze when an 'O' cannot be flipped,
+      // if it has atleast one 'O' in it's adjacent, AND ultimately this chain of adjacent 'O's is connected to some 'O' which lies on boundary of board
+        
+      //consider these two cases for clarity :
+      /*
+        O's won't be flipped          O's will be flipped
+        [X O X X X]                   [X X X X X]     
+        [X O O O X]                   [X O O O X]
+        [X O X X X]                   [X O X X X] 
+        [X X X X X]                   [X X X X X]
+      
+      So we can conclude if a chain of adjacent O's is connected some O on boundary then they cannot be flipped
+      
+      */
+        
+      //Steps to Solve :
+      //1. Move over the boundary of board, and find O's 
+      //2. Every time we find an O, perform DFS from it's position
+      //3. In DFS convert all 'O' to '#'      (why?? so that we can differentiate which 'O' can be flipped and which cannot be)   
+      //4. After all DFSs have been performed, board contains three elements,#,O and X
+      //5. 'O' are left over elements which are not connected to any boundary O, so flip them to 'X'
+      //6. '#' are elements which cannot be flipped to 'X', so flip them back to 'O'
